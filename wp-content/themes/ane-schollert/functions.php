391512c5164aee359fdf64b1accf34be4e124ee4
Helena Nielsen <hele168f@stud.kea.dk>
2022-12-14 11:14:20 +0100

    ny

--- wp-content/themes/ane-schollert/functions.php
+++ wp-content/themes/ane-schollert/functions.php
@@ -1,7 +1,11 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'enqueue_important_files' );
+add_action( 'wp_enqueue_scripts', 'enqueue_important_files', 11);
 function enqueue_important_files() {
 /*hent parent stylesheet i parenttemaets mappe*/
 wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
+wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
 }
 ?>
